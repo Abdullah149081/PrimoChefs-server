a1e749958a26908da322c6304fb9c70dc06e8d99
Abdullah149081 <masudmd425@gmail.com>
2023-05-02 23:30:27 +0600

    cors add

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefRecipes = require("./data/chefRecipes.json");
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("primoChefs is running");
 });
