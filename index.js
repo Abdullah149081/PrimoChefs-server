6a5d73572c433b5a51dd1b9938a905cf166f3e9d
Abdullah149081 <masudmd425@gmail.com>
2023-05-02 20:31:52 +0600

    chefAllData api create

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefRecipes = require("./data/chefRecipes.json");
+
 app.get("/", (req, res) => {
   res.send("primoChefs is running");
+});
+
+app.get("/chefAllData", (req, res) => {
+  res.send(chefRecipes);
 });
 
 app.listen(port, () => {
